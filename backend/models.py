199bb4bba43ebf03def8c42d5d36647865387769
Aleksandr Sokolov <algensok@gmail.com>
2024-08-02 22:34:58 +0300

    Запущен docker-контейнер с posgresql. Перенастроен django-проект на СУБД PostgreSQL. Созданы модели данных. Сделаны миграции.

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,104 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class OrderStatusChoices(models.TextChoices):
+    """
+        Статусы заказа
+    """
+    BASKET = 'OPEN', 'Статус корзины'
+    NEW = 'NEW', 'Новый'
+    CONFIRMED = 'CONFIRMED', 'Подтвержден'
+    ASSEMBLED = 'ASSEMBLED', 'Собран'
+    SENT = 'SENT', 'Отправлен'
+    DELIVERED = 'DELIVERED', 'Доставлен'
+    CANCELED = 'CANCELED', 'Отменен'
+
+
+class UserTypeChoices(models.TextChoices):
+    """
+        Типы пользователей
+    """
+    SHOP = 'SHOP', 'Магазин'
+    BUYER = 'BUYER', 'Покупатель'
+
+
+class Shop(models.Model):
+    """
+        Модель магазина
+    """
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    owner = models.OneToOneField(User, related_name='shop', on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    """
+        Модель Категории товара
+    """
+    name = models.CharField(max_length=100)
+    shops = models.ManyToManyField(Shop, related_name='categories')
+
+
+class Product(models.Model):
+    """
+        Модель товара
+    """
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+
+class ProductInfo(models.Model):
+    """
+        Модель информации о продукте
+    """
+    product = models.ForeignKey(Product, related_name='product_info', on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, related_name='product_info', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_rrc = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Parameter(models.Model):
+    """
+        Модель параметра
+    """
+    name = models.CharField(max_length=100)
+
+
+class ProductParameter(models.Model):
+    """
+        Модель параметров продукта
+    """
+    product_info = models.ForeignKey(ProductInfo, related_name='product_parameter', on_delete=models.CASCADE)
+    parameter = models.ForeignKey(Parameter, related_name='product_parameter', on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+
+
+class Order(models.Model):
+    """
+        Модель заказа
+    """
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    dt = models.DateTimeField(auto_now_add=True)
+    status = models.TextField(choices=OrderStatusChoices.choices)
+
+
+class OrderItem(models.Model):
+    """
+        Модель элемента заказа
+    """
+    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, related_name='order_items', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+
+class Contact(models.Model):
+    """
+        Модель контакта
+    """
+    user = models.ForeignKey(User, related_name='contacts', on_delete=models.CASCADE)
+    type = models.TextField(choices=UserTypeChoices.choices, default=UserTypeChoices.BUYER)
+    value = models.CharField(max_length=255)
--- orders/settings.py
+++ orders/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ond=13@p(e523(r!of@$zs6y4zl(n3(n1ra+h+cmq66i&m*n8('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +85,12 @@ WSGI_APPLICATION = 'orders.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('POSTGRES_ENGINE'),
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
